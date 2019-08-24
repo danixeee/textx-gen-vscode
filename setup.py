e17c7a020c1b55e4549aa40ebfc8d2beb09df258
Daniel Elero <danixeee@gmail.com>
2019-08-24 19:46:32 +0200

    Add ci script

--- setup.py
+++ setup.py
@@ -1,31 +1,42 @@
-# flake8: noqa
 import codecs
 import os
 
 from setuptools import find_packages, setup
 
-PACKAGE_NAME = 'textx-gen-vscode'
-VERSION = '0.1.0'
-AUTHOR = 'Daniel Elero'
-AUTHOR_EMAIL = 'danixeee@gmail.com'
-DESCRIPTION = 'a VS Code extension generator'
-KEYWORDS = 'textX DSL python domain specific languages VS Code'
-LICENSE = 'MIT'
-URL = 'https://github.com/danixeee/textx-gen-vscode'
+PACKAGE_NAME = "textx-gen-vscode"
+VERSION = "0.1.0"
+AUTHOR = "Daniel Elero"
+AUTHOR_EMAIL = "danixeee@gmail.com"
+DESCRIPTION = "a VS Code extension generator"
+KEYWORDS = "textX DSL python domain specific languages VS Code"
+LICENSE = "MIT"
+URL = "https://github.com/danixeee/textx-gen-vscode"
 
 packages = find_packages()
 
-print('packages:', packages)
+print("packages:", packages)
 
-README = codecs.open(os.path.join(os.path.dirname(__file__), 'README.md'),
-                     'r', encoding='utf-8').read()
+README = codecs.open(
+    os.path.join(os.path.dirname(__file__), "README.md"), "r", encoding="utf-8"
+).read()
+
+ci_require = ["bandit", "pytest", "pytest-cov", "pytest-azurepipelines"]
+
+dev_require = ["bandit==1.5.1"]
+
+tests_require = ["coverage==4.5.3", "pytest==4.3.1", "pytest-cov==2.6.1"]
+
+if python_version().startswith("3.6"):  # For python 3.6
+    ci_require.append("black")
+    dev_require.append("black")
+
 
 setup(
     name=PACKAGE_NAME,
     version=VERSION,
     description=DESCRIPTION,
     long_description=README,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url=URL,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
@@ -34,22 +45,19 @@ setup(
     packages=packages,
     include_package_data=True,
     install_requires=["click", "jinja2", "textx", "textx-gen-coloring"],
-    entry_points={
-        'textx_generators': [
-            'vscode_gen = textx_gen_vscode:vscode_gen'
-        ]
-    },
+    entry_points={"textx_generators": ["vscode_gen = textx_gen_vscode:vscode_gen"]},
+    extras_require={"ci": ci_require, "dev": dev_require, "test": tests_require},
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ]
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
